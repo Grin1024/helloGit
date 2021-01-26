7123281870b7de541970112335b1974c687123ec
lihui <lihui.wangx@gmail.com>
2021-01-26 19:24:04 +0800

    添加分支

--- src/index.java
+++ src/index.java
@@ -2,6 +2,7 @@ public class index {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("xxx");
+        System.out.println("hello");
     }
 }
 
